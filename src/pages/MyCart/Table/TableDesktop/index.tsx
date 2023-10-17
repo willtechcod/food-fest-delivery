d5d1a5f69352870619519e8f1d64b6f23c3c8ea5
willtechcod <dev.pereira2019@gmail.com>
2023-10-17 00:57:45 -0300

    feat: finsh cart end table orders

--- src/pages/MyCart/Table/TableDesktop/index.tsx
+++ src/pages/MyCart/Table/TableDesktop/index.tsx
@@ -1,6 +1,17 @@
-import { Container } from "./styles"
+import { FaTrashAlt } from 'react-icons/fa';
+import { useCart } from "../../../../hooks/useCart";
+
+import currencyFormat from "../../../../helpers/currencyFormat";
+
+import  plusImg  from '../../../../assets/circle-plus.svg';
+import  minusImg  from '../../../../assets/circle-minus.svg';
+
+import { Container } from "./styles";
+import { ConfirmOrder } from '../../../../components/ConfirmOrder';
 
 export function TableDesktop() {
+    const { cart, removeSnackFromCart, snackCartInCrement, snackCartDecrement } = useCart();
+
     return (
         <Container>
             <table>
@@ -13,7 +24,44 @@ export function TableDesktop() {
                         <th></th>
                     </tr>
                 </thead>
+                <tbody>
+                    {
+                        cart.map((item) => (
+                            <tr key={`${item.snack}-${item.id}`}>
+                                <td>
+                                    <img src={item.image} alt={item.name} />
+                                </td>
+                                <td>
+                                    <h4>{item.name}</h4>
+                                    <span>{currencyFormat(item.price)}</span>
+                                </td>
+                                <td>
+                                    <div>
+                                        <button type="button" onClick={() => snackCartDecrement(item)} >
+                                            <img src={minusImg} alt="botao menos" />
+                                        </button>
+
+                                        <span>{`${item.quantity}`.padStart(2, '0')}</span>
+
+                                        <button type="button" onClick={() => snackCartInCrement(item)} >
+                                            <img src={plusImg} alt="botao mais" />
+                                        </button>
+                                    </div>
+                                </td>
+                                <td>
+                                    <h5>{currencyFormat(item.subtotal)}</h5>
+                                </td>
+                                <td>
+                                    <button type='button' onClick={() => removeSnackFromCart(item)}>
+                                        <FaTrashAlt />
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
             </table>
+            <ConfirmOrder />
         </Container>
     )
 }
